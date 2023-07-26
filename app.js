027f62b4541e5005bd5ba03f49fa869f44e28968
Danil <dposyagin@gmail.com>
2023-07-26 14:51:49 +0300

    Update app.js

--- app.js
+++ app.js
@@ -2,14 +2,24 @@ let tg = window.Telegram.WebApp;
 
 tg.expand();
 
-tg.showScanQrPopup("", function(text) {
-  tg.sendData(text);
-  return true;
-            });
+    this.is_telegram_api_updated = this.TWA.isVersionAtLeast('6.4');
+    // platform not updated if version is not 6.4 or greater
 
-tg.onEvent('qrTextReceived', tg.sendData(this.data));
+    if (this.TWA.platform != "unknown") {
+      this.is_telegram_client = true;
+    }
 
-//tg.sendData('тест')
+//check that platform is telegram and version is at least 6.4
+if (tg.platform != "unknown" && tg.isVersionAtLeast('6.4')) {
+  
+  tg.showScanQrPopup("", function(text) {
+                          tg.sendData(text);
+                          return true;
+                          });
 
-//document.write('Тест метода sendData');
-
+  tg.onEvent('qrTextReceived', tg.sendData(this.data));
+} else if (tg.platform == "unknown") {
+  document.write('Это приложение следует запускать из клиента telegram');
+} else if (!tg.isVersionAtLeast('6.4')) {
+  document.write('Это приложение не может работать с клиентами telegram версии ниже 6.4. Пожалуйста, обновите ваше приложение.');
+}
