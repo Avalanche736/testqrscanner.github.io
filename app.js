159d02a1d9718abc7f68dd5dbe4c99e8f1a9862e
Danil <dposyagin@gmail.com>
2023-09-21 14:13:03 +0300

    Update app.js

--- app.js
+++ app.js
@@ -2,18 +2,21 @@ let tg = window.Telegram.WebApp;
 
 tg.expand();
 
-document.write('qr code scanner');
-
-//check that platform is telegram and version is at least 6.4
-if (tg.platform != "unknown" && tg.isVersionAtLeast('6.4')) {
-  
   tg.showScanQrPopup("", function(text) {
                           tg.sendData(text);
                           return true;
                           });
+
+//check that platform is telegram and version is at least 6.4
+// if (tg.platform != "unknown" && tg.isVersionAtLeast('6.4')) {
   
-} else if (tg.platform == "unknown") {
-  document.write('Это приложение следует запускать из клиента telegram');
-} else if (!tg.isVersionAtLeast('6.4')) {
-  document.write('Это приложение не может работать с клиентами telegram версии ниже 6.4. Пожалуйста, обновите ваше приложение.');
-}
+//   tg.showScanQrPopup("", function(text) {
+//                           tg.sendData(text);
+//                           return true;
+//                           });
+  
+// } else if (tg.platform == "unknown") {
+//   document.write('Это приложение следует запускать из клиента telegram');
+// } else if (!tg.isVersionAtLeast('6.4')) {
+//   document.write('Это приложение не может работать с клиентами telegram версии ниже 6.4. Пожалуйста, обновите ваше приложение.');
+// }
